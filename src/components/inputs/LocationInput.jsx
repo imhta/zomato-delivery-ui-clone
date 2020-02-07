4770e5c232145dfb5364f91862ae30b4bdd75a52
imhta <imhtapm@gmail.com>
2020-02-08 01:20:36 +0530

    :art: :hammer: add material icons and fine tunning

--- src/components/inputs/LocationInput.jsx
+++ src/components/inputs/LocationInput.jsx
@@ -2,22 +2,43 @@ import React from 'react'
 import styled from 'styled-components'
 
 const LocationInputWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    position: relative;
     input{
-        width: 230px;
+        width: 225px;
         border: none;
         border-radius: 2px;
-        padding: 10px 20px;
+        padding: 10px 25px;
         margin: 0px 3px;
         font-size: 14px;
         font-family: OpenSans Regular, sans-serif;
         font-weight: bold;
+        cursor: pointer;
+    }
+    .material-icons{
+        font-size: 20px;
+        position:absolute;
+        color: #89959B;
+    }
+    .material-icons.location{
+        left: 7px;
+    }
+    .material-icons.arrow{
+        right: 10px;
     }
 `
 
 function LocationInput() {
     return (
         <LocationInputWrapper>
+            <i class="material-icons location">
+                location_on
+            </i>
             <input type="text" value="Mumbai" readOnly />
+            <i class="material-icons arrow">
+                keyboard_arrow_down
+            </i>
         </LocationInputWrapper>
     )
 }
--- src/components/inputs/SearchInput.jsx
+++ src/components/inputs/SearchInput.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 
 const SearchInputWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    position: relative;
     input{
-        width: 333px;
+        width: 325px;
         border: none;
         border-radius: 2px;
-        padding: 10px 20px;
+        padding: 10px 28px;
         font-size: 14px;
         font-family: OpenSans Regular, sans-serif;
+        cursor: pointer;
     }
     button{
         background: #cb202d;
@@ -29,11 +33,21 @@ const SearchInputWrapper = styled.div`
             transition: .15s ease-out background-color;
         }
     }
+    .material-icons{
+        font-size: 18px;
+        font-weight: bold;
+        color: #89959B;
+        position: absolute;
+        left: 5px;
+    }
 `
 
 function SearchInput() {
     return (
         <SearchInputWrapper>
+            <i class="material-icons">
+                search
+            </i>
             <input type="text" placeholder="Search for restaurant or cusisines..." />
             <button>Search</button>
         </SearchInputWrapper>
