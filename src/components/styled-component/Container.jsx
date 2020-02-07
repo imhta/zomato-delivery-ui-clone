cc895b92576183bdb97d69cfbdab5d8e49ab739e
imhta <imhtapm@gmail.com>
2020-02-07 23:28:53 +0530

    :heavy_plus_sign: add container styled compoent

---
+++ src/components/styled-component/Container.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+    max-width: 1140px;
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+`
+
+
+export default Container;
