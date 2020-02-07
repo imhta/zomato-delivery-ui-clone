81e2763ffce3bee8f93dbc2954d563a0466bd5ee
imhta <imhtapm@gmail.com>
2020-02-08 01:01:28 +0530

    :heavy_plus_sign: add initial state

---
+++ src/initialState.js
@@ -0,0 +1,16 @@
+const path = [
+  {
+    title: "Home",
+    href: "https://www.zomato.com/"
+  },
+  {
+    title: "India",
+    href: "https://www.zomato.com/india"
+  },
+  {
+    title: "Mumbai Restaurants",
+    href: "https://www.zomato.com/mumbai/delivery"
+  }
+];
+
+export { path };
