f8e4944e77b5a6f4c1eecd1b1ffb758ce3afa82d
imhta <imhtapm@gmail.com>
2020-02-07 16:12:50 +0530

    basic styled component test with navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-        Hello
+        <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const NavbarWrapper = styled.nav`
+    background-color: red;
+`;
+export default function Navbar() {
+    return (
+        <NavbarWrapper>
+            Zomato
+        </NavbarWrapper>
+    )
+}
