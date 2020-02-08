52f9bc03dea428c593249d62dc7a21009bd51add
imhta <imhtapm@gmail.com>
2020-02-08 16:26:23 +0530

    :heavy_plus_sign: create sponsor and popular card

---
+++ src/components/styled-component/SAPCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-compoents'
+
+function SAPCard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SAPCard
