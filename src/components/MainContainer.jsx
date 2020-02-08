6d259eccef7bf44344b70279142dd592b8ccbdad
imhta <imhtapm@gmail.com>
2020-02-08 22:14:34 +0530

    :heavy_plus_sign: :hammer: creaate dummy Restaurants container and formater fix

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,37 +1,38 @@
-import React from 'react'
-import styled from 'styled-components'
-import Container from '../components/styled-component/Container'
-import Filters from './Filters'
-import SponAndPop from './SponAndPop'
+import React from 'react';
+import styled from 'styled-components';
+import Container from './styled-component/Container';
+import Filters from './Filters';
+import SponAndPop from './SponAndPop';
+import RestaurantsContainer from './RestaurantsContainer';
 
 
 const MainHeader = styled.h1`
     font-family: OpenSans Regular, sans-serif;
     font-size: 1.8em;
     margin: 15px;
-`
+`;
 const MainLayout = styled.main`
     display: grid;
     width: 100%;
     grid-template-columns: .5fr 1fr .60fr;
 
-`
+`;
 
 function MainContainer() {
-    return (
-        <>
-            <Container>
-                <MainHeader>Delivery Restaurants in Mumbai</MainHeader>
-            </Container>
-            <Container>
-                <MainLayout>
-                    <Filters/>
-                    <div></div>
-                    <SponAndPop />
-                </MainLayout>
-            </Container>
-        </>
-    )
+  return (
+    <>
+      <Container>
+        <MainHeader>Delivery Restaurants in Mumbai</MainHeader>
+      </Container>
+      <Container>
+        <MainLayout>
+          <Filters />
+          <RestaurantsContainer />
+          <SponAndPop />
+        </MainLayout>
+      </Container>
+    </>
+  );
 }
 
-export default MainContainer
+export default MainContainer;
---
+++ src/components/RestaurantsContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function RestaurantsContainer() {
+  return (
+    <div />
+  );
+}
+
+export default RestaurantsContainer;
