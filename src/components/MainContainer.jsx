a7e53465295b522a902a24298d5de264c4d3ed0f
imhta <imhtapm@gmail.com>
2020-02-08 16:21:59 +0530

    :art: add layout for main container and basic content to it

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
+import Container from '../components/styled-component/Container'
+import Filters from './Filters'
+import SponAndPop from './SponAndPop'
+
+
+const MainHeader = styled.h1`
+    font-family: OpenSans Regular, sans-serif;
+    font-size: 1.8em;
+    margin: 15px;
+`
+const MainLayout = styled.main`
+    display: grid;
+    width: 100%;
+    grid-template-columns: .5fr 1fr .60fr;
+
+`
 
 function MainContainer() {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Container>
+                <MainHeader>Delivery Restaurants in Mumbai</MainHeader>
+            </Container>
+            <Container>
+                <MainLayout>
+                    <Filters/>
+                    <div></div>
+                    <SponAndPop />
+                </MainLayout>
+            </Container>
+        </>
     )
 }
 
