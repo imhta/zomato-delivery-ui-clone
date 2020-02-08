31678b15a2538640ecab68b8bbdafb5e738dca0a
imhta <imhtapm@gmail.com>
2020-02-08 16:23:52 +0530

    :heavy_plus_sign: :art: create basic style and layout of  Filters

---
+++ src/components/Filters.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components'
+import CheckBox from './styled-component/CheckBox'
+import FilterList from './styled-component/FilterList'
+import { filterState } from '../initialState'
+
+const FiltersWrapper = styled.aside`
+    background-color: white;
+    margin: 1rem 0;
+    padding: 1em;
+    border-radius: .28571429rem;
+    border: 1px solid #e7e7e7;
+
+    h3{
+        font-size: 17px;
+        margin: 0px 0px 14px 0px;
+    }
+    hr{
+        margin: 10px 0px;
+        border-top: 1px solid #e7e7e7;
+        border-bottom: 1px solid rgba(255,255,255,.1);
+    }
+
+`
+
+function Filters() {
+    return (
+        <FiltersWrapper>
+            <h3>Filters</h3>
+            <section className="top-section">
+                <CheckBox title="Book a Table" /><hr/>
+                <CheckBox title="Zomato Gold Partner" /><hr/>
+                <CheckBox title="Order Food Online" />
+            </section>
+            <FilterList title="Sort by" items={filterState.sortBy}/>
+            <FilterList title="Category" items={filterState.category}/>
+            <FilterList title="Location" items={filterState.location}/>
+            <FilterList title="Cuisine" items={filterState.cuisine}/>
+            <FilterList title="Establishment Type" items={filterState.establishment}/>
+            <FilterList title="Cost for two" items={filterState.costForTwo}/>
+            <FilterList title="Restaurant Offers" checked checkItems={filterState.restaurantsOffer} />
+            <FilterList title="More filters" checked checkItems={filterState.moreFilters} />
+
+        </FiltersWrapper>
+    )
+}
+
+export default Filters
