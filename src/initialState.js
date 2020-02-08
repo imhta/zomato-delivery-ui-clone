7acb1ffbce42a5133a54f84317dd229c84fd97b2
imhta <imhtapm@gmail.com>
2020-02-08 21:20:07 +0530

    :heavy_plus_sign: :up: add delivery resturant in sap and lint fix

--- src/initialState.js
+++ src/initialState.js
@@ -1,16 +1,18 @@
+import deliveryRestaurantsIcon from './assets/images/category_1.png';
+
 const path = [
   {
-    title: "Home",
-    href: "https://www.zomato.com/"
+    title: 'Home',
+    href: 'https://www.zomato.com/',
   },
   {
-    title: "India",
-    href: "https://www.zomato.com/india"
+    title: 'India',
+    href: 'https://www.zomato.com/india',
   },
   {
-    title: "Mumbai Restaurants",
-    href: "https://www.zomato.com/mumbai/delivery"
-  }
+    title: 'Mumbai Restaurants',
+    href: 'https://www.zomato.com/mumbai/delivery',
+  },
 ];
 
 const filterState = {
@@ -34,7 +36,7 @@ const filterState = {
     {
       title: 'Recently Added',
       sub: 'new to old',
-    }
+    },
   ],
   category: [
     {
@@ -52,7 +54,7 @@ const filterState = {
     {
       title: 'Cafes',
       count: 1065,
-    }
+    },
   ],
   location: [
     {
@@ -73,16 +75,16 @@ const filterState = {
     },
     {
       title: 'Thane',
-      count: 1917
+      count: 1917,
     },
     {
       title: 'Thane Suburbs',
-      count: 1738
+      count: 1738,
     },
     {
       title: 'Andheri East',
-      count: 1402
-    }
+      count: 1402,
+    },
   ],
   cuisine: [
     {
@@ -103,24 +105,24 @@ const filterState = {
     },
     {
       title: 'Beverages',
-      count: 3567
+      count: 3567,
     },
     {
       title: 'South Indian',
-      count: 2738
+      count: 2738,
     },
     {
       title: 'Mughlai',
-      count: 2071
+      count: 2071,
     },
     {
       title: 'Biryani',
-      count: 1900
+      count: 1900,
     },
     {
       title: 'Street Food',
-      count: 1780
-    }
+      count: 1780,
+    },
   ],
   establishment: [
     {
@@ -141,30 +143,30 @@ const filterState = {
     },
     {
       title: 'Cafes',
-      count: 791
+      count: 791,
     },
     {
       title: 'Bars',
-      count: 638
+      count: 638,
     },
     {
       title: 'Beverage Shops',
-      count: 476
+      count: 476,
     },
     {
       title: 'Sweet Shops',
-      count: 402
+      count: 402,
     },
     {
       title: 'Food Courts',
-      count: 279
+      count: 279,
     },
     {
       title: 'Kiosks',
-      count: 150
-    }
+      count: 150,
+    },
   ],
-  costForTwo:  [
+  costForTwo: [
     {
       title: 'Less than ₹250',
       count: 3784,
@@ -183,16 +185,14 @@ const filterState = {
     },
     {
       title: '₹1,500 to ₹2,500',
-      count: 891
+      count: 891,
     },
     {
       title: '₹2,500 +',
-      count: 46
-    }
+      count: 46,
+    },
   ],
-  restaurantsOffer: [
-    'Promotions'
-  ],
+  restaurantsOffer: ['Promotions'],
   moreFilters: [
     'Open at 2:55 PM',
     'Alcohol served',
@@ -207,8 +207,55 @@ const filterState = {
     'Luxury Dining',
     'Outdoor seating',
     'Wheelchair Access',
-    'Sneekpeek'
-  ]
-}
+    'Sneekpeek',
+  ],
+};
 
-export { path, filterState };
+const SAP = {
+  deliveryRestaurants: {
+    title: 'Delivery Restaurants',
+    subtitle: 'Get food delivered',
+    icon: deliveryRestaurantsIcon,
+    hotels: [
+      {
+        title: 'Meraki',
+        subtitle: 'SANTACRUZ EAST, MUMBAI Taste to die for!',
+        rating: '4.0',
+        image: `https://source.unsplash.com/120x120/?food,${Math.random(1000)}`,
+      },
+      {
+        title: 'Radha Krishna',
+        subtitle: 'MAJIWADA, THANE WEST, THANE Order Now !',
+        rating: '3.4',
+        image: `https://source.unsplash.com/120x120/?food,${Math.random(1000)}`,
+      },
+      {
+        title: 'Bikkgane Briyani',
+        subtitle:
+          'VIKHROLI, MUMBAI Taste of Hyderbadi Biryani at your doorstep!',
+        rating: '4.3',
+        image: `https://source.unsplash.com/120x120/?food,${Math.random(1000)}`,
+      },
+      {
+        title: 'Subway',
+        subtitle: 'MAHAKALI, MUMBAI Best Sub',
+        rating: '3.7',
+        image: `https://source.unsplash.com/120x120/?food,${Math.random(1000)}`,
+      },
+      {
+        title: 'The Chinese Food Co',
+        subtitle:
+          'ANDHERI LOKHANDWALA, ANDHERI WEST Order now for authentic chinese food in town!',
+        rating: '4.4',
+        image: `https://source.unsplash.com/120x120/?food,${Math.random(1000)}`,
+      },
+      {
+        title: 'Lazeez-Good Mood Food',
+        subtitle: 'KASARVADAVLI,THANE WEST,THANE Order Food Now',
+        rating: '4.0',
+        image: `https://source.unsplash.com/120x120/?food,${Math.random(1000)}`,
+      },
+    ],
+  },
+};
+export { path, filterState, SAP };
