42ac53db2158a65de73196be610464e97f61795a
imhta <imhtapm@gmail.com>
2020-02-10 15:13:40 +0530

    :heavy_plus_sign: add more state to initial state

--- src/initialState.js
+++ src/initialState.js
@@ -1,4 +1,8 @@
 import deliveryRestaurantsIcon from './assets/images/category_1.png';
+import dineOutIcon from './assets/images/category_3.png';
+import cafeIcon from './assets/images/category_6.png';
+import iceCreamIcon from './assets/images/category_8.png';
+import pubBarIcon from './assets/images/category_9.png';
 import randomImageUrl from './util';
 
 const path = [
@@ -216,12 +220,192 @@ const SAP = {
   deliveryRestaurants: {
     title: 'Delivery Restaurants',
     subtitle: 'Get food delivered',
-    icon: deliveryRestaurantsIcon,
+    icon: dineOutIcon,
     hotels: [
       {
         title: 'Meraki',
         subtitle: 'SANTACRUZ EAST, MUMBAI Taste to die for!',
         rating: '4.0',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'Radha Krishna',
+        subtitle: 'MAJIWADA, THANE WEST, THANE Order Now !',
+        rating: '3.4',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'Bikkgane Briyani',
+        subtitle:
+          'VIKHROLI, MUMBAI Taste of Hyderbadi Biryani at your doorstep!',
+        rating: '4.3',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'Subway',
+        subtitle: 'MAHAKALI, MUMBAI Best Sub',
+        rating: '3.7',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'The Chinese Food Co',
+        subtitle:
+          'ANDHERI LOKHANDWALA, ANDHERI WEST Order now for authentic chinese food in town!',
+        rating: '4.4',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'Lazeez-Good Mood Food',
+        subtitle: 'KASARVADAVLI,THANE WEST,THANE Order Food Now',
+        rating: '4.0',
+        image: randomImageUrl(),
+      },
+    ],
+  },
+  dineOut: {
+    title: 'Dine-out restaurants',
+    subtitle: 'Go out for a meal',
+    icon: dineOutIcon,
+    hotels: [
+      {
+        title: 'Meraki',
+        subtitle: 'SANTACRUZ EAST, MUMBAI Taste to die for!',
+        rating: 'NEW',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'Radha Krishna',
+        subtitle: 'MAJIWADA, THANE WEST, THANE Order Now !',
+        rating: '3.4',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'Bikkgane Briyani',
+        subtitle:
+          'VIKHROLI, MUMBAI Taste of Hyderbadi Biryani at your doorstep!',
+        rating: '4.3',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'Subway',
+        subtitle: 'MAHAKALI, MUMBAI Best Sub',
+        rating: '3.7',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'The Chinese Food Co',
+        subtitle:
+          'ANDHERI LOKHANDWALA, ANDHERI WEST Order now for authentic chinese food in town!',
+        rating: '4.4',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'Lazeez-Good Mood Food',
+        subtitle: 'KASARVADAVLI,THANE WEST,THANE Order Food Now',
+        rating: '4.0',
+        image: randomImageUrl(),
+      },
+    ],
+  },
+  cafe: {
+    title: 'Cafes',
+    subtitle: 'Coffee, snacks & beverages',
+    icon: cafeIcon,
+    hotels: [
+      {
+        title: 'Meraki',
+        subtitle: 'SANTACRUZ EAST, MUMBAI Taste to die for!',
+        rating: 'NEW',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'Radha Krishna',
+        subtitle: 'MAJIWADA, THANE WEST, THANE Order Now !',
+        rating: '3.4',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'Bikkgane Briyani',
+        subtitle:
+          'VIKHROLI, MUMBAI Taste of Hyderbadi Biryani at your doorstep!',
+        rating: '4.3',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'Subway',
+        subtitle: 'MAHAKALI, MUMBAI Best Sub',
+        rating: '3.7',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'The Chinese Food Co',
+        subtitle:
+          'ANDHERI LOKHANDWALA, ANDHERI WEST Order now for authentic chinese food in town!',
+        rating: '4.4',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'Lazeez-Good Mood Food',
+        subtitle: 'KASARVADAVLI,THANE WEST,THANE Order Food Now',
+        rating: '4.0',
+        image: randomImageUrl(),
+      },
+    ],
+  },
+  iceCream: {
+    title: 'Cakes, ice cream & bakery',
+    subtitle: 'Satisfy your sweet tooth',
+    icon: iceCreamIcon,
+    hotels: [
+      {
+        title: 'Meraki',
+        subtitle: 'SANTACRUZ EAST, MUMBAI Taste to die for!',
+        rating: 'NEW',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'Radha Krishna',
+        subtitle: 'MAJIWADA, THANE WEST, THANE Order Now !',
+        rating: '3.4',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'Bikkgane Briyani',
+        subtitle:
+          'VIKHROLI, MUMBAI Taste of Hyderbadi Biryani at your doorstep!',
+        rating: '4.3',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'Subway',
+        subtitle: 'MAHAKALI, MUMBAI Best Sub',
+        rating: '3.7',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'The Chinese Food Co',
+        subtitle:
+          'ANDHERI LOKHANDWALA, ANDHERI WEST Order now for authentic chinese food in town!',
+        rating: '4.4',
+        image: randomImageUrl(),
+      },
+      {
+        title: 'Lazeez-Good Mood Food',
+        subtitle: 'KASARVADAVLI,THANE WEST,THANE Order Food Now',
+        rating: '4.0',
+        image: randomImageUrl(),
+      },
+    ],
+  },
+  pubBar: {
+    title: 'Pubs, bars & party spots',
+    subtitle: 'Enjoy the nightlife',
+    icon: pubBarIcon,
+    hotels: [
+      {
+        title: 'Meraki',
+        subtitle: 'SANTACRUZ EAST, MUMBAI Taste to die for!',
+        rating: 'NEW',
         image: randomImageUrl(),
       },
       {
@@ -264,10 +448,10 @@ const restaurants = [
     mainImg: 'https://b.zmtcdn.com/data/pictures/chains/7/46577/8b503cd43b786fc3be3dfa40fc3a2ca3_featured_v2.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A',
     isSponsored: true,
     types: ['Dine'],
-    name: 'React restaurant',
+    name: 'Meraki',
     miniLocation: 'Powai',
     rating: '4.2',
-    votes: '2001',
+    votes: 2001,
     address: 'ANDHERI LOKHANDWALA, ANDHERI WEST',
     moreDetails: [
       {
@@ -306,6 +490,288 @@ const restaurants = [
         icon: 'shopping_cart',
         textColor: 'white',
         bgColor: '#099E44',
+      },
+    ],
+    hasOutlet: true,
+    outlets: [
+      {
+        location: 'Airport',
+        outletImg: randomImageUrl('hotel'),
+      },
+      {
+        location: 'Railway Road',
+        outletImg: randomImageUrl('hotel'),
+      },
+    ],
+    noOfOutlets: 11,
+  },
+  {
+    mainImg: randomImageUrl(),
+    isSponsored: true,
+    types: ['Dine'],
+    name: 'Meraki',
+    miniLocation: 'Powai',
+    rating: '4.2',
+    votes: 2001,
+    address: 'ANDHERI LOKHANDWALA, ANDHERI WEST',
+    moreDetails: [
+      {
+        key: 'CUISINES',
+        value: 'American, Mediterranean, Italian, European',
+      },
+      {
+        key: 'COST FOR TWO',
+        value: '₹3,500',
+      },
+      {
+        key: 'HOURS',
+        value: '8pm – 1am (Mon-Fri),12:30pm – 3:30pm, 8pm –...',
+      },
+      {
+        key: 'FEATURED IN',
+        value: 'Romantic',
+      },
+    ],
+    availableActions: [
+      {
+        action: 'Call',
+        icon: 'call',
+      },
+      {
+        action: 'View Menu',
+        icon: 'book',
+      },
+      {
+        action: 'Book a Table',
+        icon: 'calendar_today',
+        textColor: '#00AACD',
+      },
+      {
+        action: 'Order',
+        icon: 'shopping_cart',
+        textColor: 'white',
+        bgColor: '#099E44',
+      },
+    ],
+    hasOutlet: true,
+    outlets: [
+      {
+        location: 'Airport',
+        outletImg: randomImageUrl('hotel'),
+      },
+      {
+        location: 'Railway Road',
+        outletImg: randomImageUrl('hotel'),
+      },
+    ],
+    noOfOutlets: 11,
+  },
+  {
+    mainImg: randomImageUrl(),
+    isSponsored: false,
+    types: ['Dine'],
+    name: 'Meraki',
+    miniLocation: 'Powai',
+    rating: '4.2',
+    votes: 2001,
+    address: 'ANDHERI LOKHANDWALA, ANDHERI WEST',
+    moreDetails: [
+      {
+        key: 'CUISINES',
+        value: 'American, Mediterranean, Italian, European',
+      },
+      {
+        key: 'COST FOR TWO',
+        value: '₹3,500',
+      },
+      {
+        key: 'HOURS',
+        value: '8pm – 1am (Mon-Fri),12:30pm – 3:30pm, 8pm –...',
+      },
+      {
+        key: 'FEATURED IN',
+        value: 'Romantic',
+      },
+    ],
+    availableActions: [
+      {
+        action: 'Call',
+        icon: 'call',
+      },
+      {
+        action: 'View Menu',
+        icon: 'book',
+      },
+      {
+        action: 'Book a Table',
+        icon: 'calendar_today',
+        textColor: '#00AACD',
+      },
+      {
+        action: 'Order',
+        icon: 'shopping_cart',
+        textColor: 'white',
+        bgColor: '#099E44',
+      },
+    ],
+    hasOutlet: false,
+  },
+  {
+    mainImg: randomImageUrl(),
+    isSponsored: true,
+    types: ['Dine'],
+    name: 'Donut',
+    miniLocation: 'Powai',
+    rating: '4.5',
+    votes: 2021,
+    address: 'LOKHANDWALA, ANDHERI WEST',
+    moreDetails: [
+      {
+        key: 'CUISINES',
+        value: 'American, Mediterranean, Italian, European',
+      },
+      {
+        key: 'COST FOR TWO',
+        value: '₹3,500',
+      },
+      {
+        key: 'HOURS',
+        value: '8pm – 1am (Mon-Fri),12:30pm – 3:30pm, 8pm –...',
+      },
+      {
+        key: 'FEATURED IN',
+        value: 'Romantic',
+      },
+    ],
+    availableActions: [
+      {
+        action: 'Call',
+        icon: 'call',
+      },
+      {
+        action: 'View Menu',
+        icon: 'book',
+      },
+      {
+        action: 'Order',
+        icon: 'shopping_cart',
+        textColor: 'white',
+        bgColor: '#099E44',
+        currentlyNotAvailable: false,
+      },
+    ],
+    hasOutlet: true,
+    outlets: [
+      {
+        location: 'Airport',
+        outletImg: randomImageUrl('hotel'),
+      },
+      {
+        location: 'Railway Road',
+        outletImg: randomImageUrl('hotel'),
+      },
+    ],
+    noOfOutlets: 11,
+  },
+  {
+    mainImg: randomImageUrl(),
+    isSponsored: true,
+    types: ['Dine'],
+    name: 'Donut',
+    miniLocation: 'Powai',
+    rating: '4.5',
+    votes: 2021,
+    address: 'LOKHANDWALA, ANDHERI WEST',
+    moreDetails: [
+      {
+        key: 'CUISINES',
+        value: 'American, Mediterranean, Italian, European',
+      },
+      {
+        key: 'COST FOR TWO',
+        value: '₹3,500',
+      },
+      {
+        key: 'HOURS',
+        value: '8pm – 1am (Mon-Fri),12:30pm – 3:30pm, 8pm –...',
+      },
+      {
+        key: 'FEATURED IN',
+        value: 'Romantic',
+      },
+    ],
+    availableActions: [
+      {
+        action: 'Call',
+        icon: 'call',
+      },
+      {
+        action: 'View Menu',
+        icon: 'book',
+      },
+      {
+        action: 'Order',
+        icon: 'shopping_cart',
+        textColor: 'white',
+        bgColor: '#099E44',
+        currentlyNotAvailable: false,
+      },
+    ],
+    hasOutlet: true,
+    outlets: [
+      {
+        location: 'Airport',
+        outletImg: randomImageUrl('hotel'),
+      },
+      {
+        location: 'Railway Road',
+        outletImg: randomImageUrl('hotel'),
+      },
+    ],
+    noOfOutlets: 11,
+  },
+  {
+    mainImg: randomImageUrl(),
+    isSponsored: true,
+    types: ['Dine'],
+    name: 'Donut',
+    miniLocation: 'Powai',
+    rating: '4.5',
+    votes: 2021,
+    address: 'LOKHANDWALA, ANDHERI WEST',
+    moreDetails: [
+      {
+        key: 'CUISINES',
+        value: 'American, Mediterranean, Italian, European',
+      },
+      {
+        key: 'COST FOR TWO',
+        value: '₹3,500',
+      },
+      {
+        key: 'HOURS',
+        value: '8pm – 1am (Mon-Fri),12:30pm – 3:30pm, 8pm –...',
+      },
+      {
+        key: 'FEATURED IN',
+        value: 'Romantic',
+      },
+    ],
+    availableActions: [
+      {
+        action: 'Call',
+        icon: 'call',
+      },
+      {
+        action: 'View Menu',
+        icon: 'book',
+      },
+      {
+        action: 'Order',
+        icon: 'shopping_cart',
+        textColor: 'white',
+        bgColor: '#099E44',
+        currentlyNotAvailable: false,
       },
     ],
     hasOutlet: true,
